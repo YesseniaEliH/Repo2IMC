b65979f3dd1a7ba62de7742104226a2225c94419
YesseniaHa <yesel1601@hotmail.com>
2017-03-05 14:45:09 -0500

    Commit by Yess

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
 function calculoIMC(){
+  var nombre = document.getElementById('nombre').value;
+  var peso = parseFloat(document.getElementById('peso').value);
+  var altura = parseFloat(document.getElementById('altura').value);
+  var imc = (peso / Math.pow(altura,2)).toFixed(2);
 
+  document.getElementById('resultado').innerHTML = nombre + " tu Indice de Masa Corporal es " + imc;
 }
